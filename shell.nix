8a5f16532ec506bff339d6abe08319bbe02b7cad
LucasNT <lucasntakahashi@gmail.com>
2025-03-23 10:23:54 -0300

    add my config, and some patches

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+pkgs.mkShell {
+  packages = with pkgs; [
+    go
+    wlroots_0_18
+    libinput
+    libxkbcommon
+    pkg-config
+    xorg.libxcb
+    xwayland
+    wayland
+    wayland-protocols
+    wayland-scanner
+    xorg.xcbutilwm
+    pixman
+  ];
+}
