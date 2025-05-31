f9ec1eccf61fa9423b4d4158eb3fb6adf5c66726
LucasNT <lucasntakahashi@gmail.com>
2025-05-30 21:38:11 -0300

    fix(nix): renaming package

--- flake.nix
+++ flake.nix
@@ -4,12 +4,14 @@
   outputs = { self, nixpkgs }:
     let pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in {
-      packages.x86_64-linux.teste-dwl =
+      packages.x86_64-linux.dwl =
         pkgs.callPackage ./default.nix { path = "${self}"; };
       devShell.x86_64-linux = pkgs.callPackage ./default.nix {
         path = "${self}";
         dev-shell = true;
       };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.teste-dwl;
+
     };
 
 }
